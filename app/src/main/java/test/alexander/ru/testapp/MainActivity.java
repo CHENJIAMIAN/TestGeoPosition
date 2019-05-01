ebed741086b631c390b3f3cbe4a50635b084a163
jiamianchen <571574085@qq.com>
2019-05-01 16:24:14 +0800

    16点22分

--- app/src/main/java/test/alexander/ru/testapp/MainActivity.java
+++ app/src/main/java/test/alexander/ru/testapp/MainActivity.java
@@ -1,37 +1,88 @@
 package test.alexander.ru.testapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button ButtonGeo;
+    TextView locationText;
+    private LocationManager locationManager;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(locationListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButtonGeo = (Button) findViewById(R.id.buttonGeo);
+        locationText = (TextView) findViewById(R.id.Location);
 
-        View.OnClickListener onClickButtonGeo = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToActivity(v);
-            }
-        };
-
-        ButtonGeo.setOnClickListener(onClickButtonGeo);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 
-    public void goToActivity(View v) {
-        Intent intent = new Intent(this, Activity_Geo.class);
+    private LocationListener locationListener = new LocationListener() {
 
-        startActivity(intent);
+        @Override
+        public void onLocationChanged(Location location) {
 
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+    };
+
+    private void showLocation(Location location) {
+        if (location != null) {
+            locationText.setText(formatLocation(location));
+        }
+
+
+    }
+
+    private String formatLocation(Location location) {
+        if (location == null) {
+            return "";
+        }
+
+        String resultStr = location.getLatitude()
+                + " , " + location.getLongitude() + "\n" + String.format("%1$tF %1$tT", new Date(
+                location.getTime())) + "\n " + location.getProvider();
+
+        return resultStr;
     }
 }
